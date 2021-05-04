a987ee12e7d793910d2c3f6128816f1e96f9dd47
Felix RABADAN <felix.rabadan@etu.univ-nantes.fr>
2021-05-04 16:46:22 +0200

    abandon stategie 1, debut strategie 2 basique

--- StrategyDummy/src/StrategyDummy.cpp
+++ StrategyDummy/src/StrategyDummy.cpp
@@ -1,23 +1,79 @@
 #include "StrategyDummy.h"
+#include <vector>
+#include <iostream>
 
 StrategyDummy::StrategyDummy(unsigned int id, unsigned int nbPlayer, const SMap* map) :
 	Id(id),
 	NbPlayer(nbPlayer)
 {
 	// faire une copie enti�re de la structure map localement dans l'objet Map
-	Map.cells = nullptr;
-	Map.nbCells = 0;
+	Map.cells = new SCell[map->nbCells];
+	Map.nbCells = map->nbCells;
+	std::cout << map->nbCells << std::endl;//on actualise les cells de la maps
+	for (unsigned int i = 0; i < Map.nbCells; ++i) {
+		Map.cells[i].infos = map->cells[i].infos; 
+		Map.cells[i].nbNeighbors = map->cells[i].nbNeighbors; 
+		Map.cells[i].neighbors = new pSCell[Map.cells[i].nbNeighbors];
+		for (unsigned int j = 0; j < Map.cells[i].nbNeighbors; ++j) Map.cells[i].neighbors[j] = map->cells[i].neighbors[j];
+	}
 }
 
 StrategyDummy::~StrategyDummy()
 {
 	// d�truire proprement la structure Map
+	delete[] Map.cells;
 }
 
 bool StrategyDummy::PlayTurn(unsigned int gameTurn, const SGameState* state, STurn* turn)
 {
 	// d�rouler votre algorithme de choix
-	return(false);
+	std::cout << state->nbCells << std::endl;
+
+	std::cout << "Map.nbCells" << Map.nbCells;
+	for (unsigned int i = 0; i < Map.nbCells; ++i) {
+		Map.cells[i].infos = state->cells[i];//on acutalise les nouvelles infos du tour
+	}
+	std::vector<pSCell> cellsPlayable;
+	for (unsigned i = 0; i < Map.nbCells; ++i) {//permet de trouver tous les cases jouables
+		if (Map.cells[i].infos.owner == Id && Map.cells[i].infos.nbDices > 1) {
+			for (unsigned j = 0; j < Map.cells[i].nbNeighbors; ++j) {//au moins une case enemi
+				if (Map.cells[i].neighbors[j]->infos.owner != Id) {
+					cellsPlayable.push_back(&Map.cells[i]);//on rajoute ladresse des cells qui sont jouables
+					break;
+				}
+			}
+		}
+	}
+	if (cellsPlayable.empty()) {
+		return false;//si aucune case jouable, on finit le tour
+		std::cout << "end of turn : no more playable cell" << std::endl;
+	}
+
+	//strategie basique, from du cellsPlayable ayant le plus de des
+	auto& From = cellsPlayable[0];
+	for (auto& it : cellsPlayable) {
+		if (it->infos.nbDices > From->infos.nbDices) {//verifie que cette case nous appartient et que elle a plus de des que la precedente
+			From = it;
+		}
+	}
+	//on a donc la case jouable avec le plus de des
+	std::cout << "From nb dices" << From->infos.nbDices << std::endl;
+
+	//	auto& From = Map.cells[turn->cellFrom];
+	//	unsigned int i = 0;
+	//	while ( i < From.nbNeighbors && From.neighbors[i]->infos.owner == Id ) i++;//trouve la premiere case qui nous appartient pas possibilité qui y ait pas de voisin donc faute
+	//	turn->cellTo = i;
+	//	std::cout << "From.nb" << From.nbNeighbors << std::endl;
+	//	for (i; i < From.nbNeighbors; i++)
+	//	{
+	//		if (From.neighbors[i]->infos.id != Id && From.neighbors[i]->infos.nbDices < From.infos.nbDices && From.neighbors[i]->infos.nbDices < From.neighbors[turn->cellTo]->infos.nbDices) {//verifie que cette case nous appartient pas et que elle a plus de des que la precedente
+	//			turn->cellTo = i;
+	//		}
+	//
+	//	}
+	//	std::cout << "fin de choix : from " << From.infos.id << " " << turn->cellFrom << std::endl;
+	//	std::cout << "to " << turn->cellTo;
+	//	return(true);
 }
 
 
